dad98cb2624484b39fc6647774e7a0ad1dff7abd
kristiankos <k.koskinen@live.fi>
2020-10-29 11:03:16 +0200

    Initial commit.

---
+++ src/main/java/com/example/ohjelmistoprojekti1/Ohjelmistoprojekti1Application.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistoprojekti1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ohjelmistoprojekti1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ohjelmistoprojekti1Application.class, args);
+	}
+
+}
