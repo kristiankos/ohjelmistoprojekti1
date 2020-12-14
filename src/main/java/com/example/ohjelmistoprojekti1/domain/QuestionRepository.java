dfd41d50ba40086b24b2e159bc22121d95c0ca47
Kristian Koskinen <k.koskinen@live.fi>
2020-12-14 15:22:59 +0200

    Muokkailua.

--- src/main/java/com/example/ohjelmistoprojekti1/domain/QuestionRepository.java
+++ src/main/java/com/example/ohjelmistoprojekti1/domain/QuestionRepository.java
@@ -1,9 +1,13 @@
 package com.example.ohjelmistoprojekti1.domain;
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+	List<Question> findBySurveyId(Long surveyId);
 
 	
 
